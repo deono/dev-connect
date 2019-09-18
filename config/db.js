bad381d52225c50c6dde6e93e659ed240546a929
Deon Odendaal <deon.odendaal@protonmail.com>
2019-09-18 06:07:12 +0100

    get all profiles and profile by user id

--- config/db.js
+++ config/db.js
@@ -1,15 +1,16 @@
-const mongoose = require("mongoose");
-const config = require("config");
-const db = config.get("mongoURI");
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true
+      useCreateIndex: true,
+      useFindAndModify: false
     });
-    console.log(">>>> [MONGO DB] connected... ");
+    console.log('>>>> [MONGO DB] connected... ');
   } catch (err) {
     console.error(err.message);
     // exit process with failure
